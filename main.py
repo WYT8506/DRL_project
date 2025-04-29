5f767488d01cc0bcc7f449e9c0388cca1c5d1b13
WYT8506 <dongshanbo123@gmail.com>
2025-04-29 15:19:21 -0400

    update

--- main.py
+++ main.py
@@ -200,10 +200,11 @@ if __name__ == '__main__':
             #Update PPO agent
             #if losses.min()/(len(dataset)) < best_suffix_loss:
             new_loss = losses.min()/(len(dataset))
-            new_loss_extended = extended_losses.min()/(len(dataset))
+            
             if PPO_action == 0:
                 PPO_reward = min(1,best_suffix_loss-(new_loss))/(end_logits_time-start_logits_time)
             else:
+                new_loss_extended = extended_losses.min()/(len(dataset))
                 PPO_reward = min(1,best_suffix_loss-(new_loss_extended))/(end_logits_time-start_logits_time)
 
             PPO_states.append(state_tensor)
